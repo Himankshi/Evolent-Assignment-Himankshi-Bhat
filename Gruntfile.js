d780a2b6a6dda9ae46a645ec1809b1bdf4b2d9b0
Dhaval Mashruwala <sarju.hansaliya@gmail.com>
2018-08-05 20:31:07 -0400

    make it better

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,9 +67,9 @@ module.exports = function(grunt) {
 		// The actual grunt server settings
 		connect: {
 			options: {
-				port: 9000,
+				port: process.env.PORT,
 				// Change this to '0.0.0.0' to access the server from outside.
-				hostname: '0.0.0.0',
+				hostname: 'localhost',
 				livereload: 35729
 			},
 			livereload: {
